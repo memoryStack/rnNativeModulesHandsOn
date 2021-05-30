77c8342f90457b54c3c8c591158f26868fa77bbb
Anuj Rao <anuj.rao@myntra.com>
2021-05-30 15:52:45 +0530

    correct the module name. And NativeModule functioality has been tested. working fine.

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { NativeModules, Button } from 'react-native';
 
-const { CalendarModule } = NativeModules;
+const { CalenderModule } = NativeModules;
 
 const NewModuleButton = () => {
   const onPress = () => {
     console.log('We will invoke the native module here!');
-    CalendarModule.createCalendarEvent('testName', 'testLocation');
+    CalenderModule.createCalendarEvent('testName', 'testLocation');
   };
 
   return (
